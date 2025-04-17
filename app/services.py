db3e223ad7dd8227531e5042b74b3e84d333292a
Philip Smith <philip@randomsmiths.com>
2025-04-17 13:16:49 -0400

    add timeout

--- app/services.py
+++ app/services.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify, render_template
 import simpleaudio as sa
+import time
 
 app = Flask(__name__)
+
+last_time = time.time()
 
 @app.route("/")
 def index():
@@ -9,6 +12,9 @@ def index():
 
 @app.route("/doorbell/")
 def ring_doorbell():
+    if time.time() - last_time() > 1:
+        return
+    last_time = time.time()
     wave_obj = sa.WaveObject.from_wave_file("app/doorbell-223669.wav")
     play_obj = wave_obj.play()
     return jsonify({'message': 'Ding Dong!', 'status': 200})
